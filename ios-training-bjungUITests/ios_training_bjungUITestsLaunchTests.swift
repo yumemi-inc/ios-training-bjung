6667b7045016783e9ba6d3e6ac64a7abf8fbdf72
bogeun jung <b_jung@yumemi.co.jp>
2023-11-28 14:43:04 +0900

    project init

---
+++ ios-training-bjungUITests/ios_training_bjungUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_training_bjungUITestsLaunchTests.swift
+//  ios-training-bjungUITests
+//
+//  Created by 鄭 普勤 on 2023/11/28.
+//
+
+import XCTest
+
+final class ios_training_bjungUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
