c1751e555e766ab0c12887e2a873e03057b5a7ae
bogeun jung <b_jung@yumemi.co.jp>
2023-11-30 17:16:26 +0900

    YumemiWeather から fetchWeatherCondition を持ってくる

--- ios-training-bjung/ViewController.swift
+++ ios-training-bjung/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
-
+    
+    let weatherRepository = YumemiWeather.self
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onReloadButtonClick(_ sender: UIButton) {
+        executeLoadData()
+    }
+    
+    func executeLoadData() {
+        Task {
+            do {
+                let result = try await loadWeatherData()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func loadWeatherData() async throws -> String {
+        return weatherRepository.fetchWeatherCondition()
+    }
 }
 
