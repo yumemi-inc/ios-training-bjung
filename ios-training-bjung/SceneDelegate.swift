574b94e952c295492b19a8af4edb5bb7bc98a124
bogeun jung <b_jung@yumemi.co.jp>
2023-12-05 11:54:26 +0900

    プロジェクト設定で Main Storyboard のベースファイルネーム指定を削除し、 Main.storyboard ファイル名を HomeView に修正

--- ios-training-bjung/SceneDelegate.swift
+++ ios-training-bjung/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let storyboard = UIStoryboard(name: "HomeView", bundle: nil)
         
         let model = WeatherModel(yumemiWeather: YumemiWeather.self)
         let presenter = HomePresenter(model: model)
