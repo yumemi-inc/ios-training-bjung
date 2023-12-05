6c40bc84199eff8e34551e878323ba9859ec6d4b
bogeun jung <b_jung@yumemi.co.jp>
2023-12-05 14:36:31 +0900

    FirstView を追加

---
+++ ios-training-bjung/FirstViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  ios-training-bjung
+//
+//  Created by 鄭 普勤 on 2023/12/05.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
