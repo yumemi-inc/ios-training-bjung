901ba587d711b65c271d2b376a22cd5a5cd225b7
bogeun jung <b_jung@yumemi.co.jp>
2024-01-23 13:38:25 +0900

    不要、存在しないテストを削除

--- ios-training-bjungTests/ios_training_bjungTests.swift
+++ ios-training-bjungTests/ios_training_bjungTests.swift
@@ -142,11 +142,5 @@ final class ios_training_bjungTests: XCTestCase {
         // Verify
         XCTAssertEqual(result, expect)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 }
